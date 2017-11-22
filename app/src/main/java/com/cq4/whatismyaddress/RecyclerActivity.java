0bc5b55bfc2940375df98e131aba72b9128cbed6
Melina Gonzalez <melinag1992@gmail.com>
2017-11-21 20:53:52 -0500

    Part01

---
+++ app/src/main/java/com/cq4/whatismyaddress/RecyclerActivity.java
@@ -0,0 +1,10 @@
+package com.cq4.whatismyaddress;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by melina.gonzalez on 11/21/17.
+ */
+
+public class RecyclerActivity extends AppCompatActivity {
+}
